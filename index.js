f8e21b19844432eacf46d8378558abb715e625a8
mokh20 <mohammad1379kh19@gmail.com>
2022-03-09 18:53:48 +0330

    addCartTotal

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const shopCartBtn = document.querySelector(".shop-cart");
 const boxShopCart = document.querySelector(".box-shop-cart");
 const backDrop = document.querySelector(".back-drop");
 const clearCart = document.querySelector(".clear-cart");
+const cartTotalProduct = document.querySelector(".total-price");
+const cartNumber = document.querySelector(".cart-number");
 
+let cart = [];
 // Get Products
 class Products {
     getProducts() {
@@ -32,11 +35,53 @@ class UI {
             productDOM.innerHTML = productBox;
         });
     }
+    addProductToCart() {
+        const productsBtns = document.querySelectorAll(".add-product");
+        const buttons = [...productsBtns];
+        buttons.forEach((Btn) => {
+            const idBtn = Btn.dataset.id;
+            const isInCart = cart.find((p) => {
+                p.id === parseInt(idBtn);
+            });
+            if (isInCart) {
+                Btn.innerText = "In Cart";
+                Btn.disabled = true;
+            }
+            Btn.addEventListener("click", (productBtn) => {
+                productBtn.target.innerText = "In Cart";
+                Btn.disabled = true;
+                // get product storage
+                const addedProducts = Storage.getProductsStorage(idBtn);
+                // add to cart
+                cart = [...cart, {...addedProducts, quantity: 1 }];
+                // save to storage
+                Storage.saveToCart(cart);
+                // update cart value
+                this.totalPriceCart(cart);
+            });
+        });
+    }
+    totalPriceCart(cart) {
+        let numberProduct = 0
+        const totalPrice = cart.reduce((acc, curr) => {
+            numberProduct += curr.quantity;
+            return acc + curr.quantity * curr.price
+        }, 0);
+        cartTotalProduct.innerText = `Total Price : ${totalPrice.toFixed(2)} $`;
+        cartNumber.innerText = numberProduct
+    }
 }
 // Storage
 class Storage {
     static saveStorage(product) {
         localStorage.setItem("products", JSON.stringify(product));
+    }
+    static getProductsStorage(id) {
+        const productsList = JSON.parse(localStorage.getItem("products"));
+        return productsList.find((product) => product.id === parseInt(id));
+    }
+    static saveToCart(cart) {
+        localStorage.setItem("Cart", JSON.stringify(cart));
     }
 }
 
@@ -46,6 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const productsData = products.getProducts();
     const ui = new UI();
     ui.displayProducts(productsData);
+    ui.addProductToCart();
     Storage.saveStorage(productsData);
 });
 
@@ -55,7 +101,7 @@ clearCart.addEventListener("click", closeShopCart);
 
 // Functions
 function showShopCart() {
-    boxShopCart.style.display = "flex";
+    boxShopCart.style.display = "grid";
     boxShopCart.style.visibility = "visible";
     backDrop.style.display = "block";
     backDrop.style.visibility = "visible";
